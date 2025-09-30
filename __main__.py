b833676db9a18a8f0651e86ad23df57a0040307d
Jakub Starzyk <jstarzyk98@gmail.com>
2025-09-30 17:43:59 +0200

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,31 @@
+import os, sys, string, weasyprint
+
+h = string.Template(
+"""\
+<html>
+<head>
+<style>
+body {
+margin: 0;
+}
+@page {
+margin: 0;
+size: ${width}px ${height}px;
+}
+</style>
+</head>
+<body>
+<a href="${link}">
+<img src="file://${image}">
+</a>
+</body>
+</html>
+"""
+)
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as data:
+        for line in data:
+            v = dict(zip(["image", "width", "height", "link"], line.rstrip().split("\t")))
+            v["image"] = os.path.realpath(v["image"])
+            weasyprint.HTML(string=h.substitute(v)).write_pdf(v["image"] + ".pdf")
